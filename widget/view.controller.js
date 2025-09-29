c52cf8a69168e056e678be69bf0da14ae4923ef4
pranali <pchavan@fortinet.com>
2025-09-29 15:16:26 +0530

    1195858- seggregation done for tags order

--- widget/view.controller.js
+++ widget/view.controller.js
@@ -129,7 +129,11 @@ Copyright end */
         return deferred.promise;
       };
 
-  
+      //to keep URL and Tags sorted 
+      $scope.sortTags = function (item) {
+        return item.structureSelected === 'URL' ? Number.MAX_VALUE : item.structureSelected;
+      };
+
       function changeURLTagsSchema(_tagsData, navigationModule) {
         const deferred = $q.defer(); // Create a master deferred
         if (!Array.isArray(_tagsData)) return;
